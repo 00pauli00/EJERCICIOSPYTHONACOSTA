f4d8dc86597a3d6a024a5ff114bb5f18e134af61
00pauli00 <paulaandreaacosta23@gmail.com>
2024-04-18 11:27:34 -0500

    commit1

---
+++ ejercicio 2.py
@@ -0,0 +1,25 @@
+A=int(input("digite el valor de la A:"))
+B=int(input("digite el valor de la B:"))
+suma= A+B
+print("la suma es", suma)
+
+
+
+C=int(input("digite el valor de la C:"))
+D=int(input("digite el valor de la D:"))
+resta= C-D
+print("la resta es", resta)
+
+
+
+E=int(input("digite el valor de la E:"))
+F=int(input("digite el valor de la F:"))
+multiplicacion= E*F
+print("la multiplicacion es", multiplicacion)
+
+
+
+G=int(input("digite el valor de la G:"))
+H=int(input("digite el valor de la H:"))
+division= G/H
+print("la division es", division)
