69ba0cfd8968c4459a2b4f669dbd06b2558ef2b9
00pauli00 <paulaandreaacosta23@gmail.com>
2024-04-18 10:50:44 -0500

    commit1

---
+++ ejercicios.py
@@ -0,0 +1,25 @@
+A=2
+B=3
+suma= A+B
+print("la suma es", suma)
+
+
+
+C=10
+D=15
+resta= C-D
+print("la resta es", resta)
+
+
+
+E=5
+F=5
+multiplicacion= E*F
+print("la multiplicacion es", multiplicacion)
+
+
+
+G=6
+H=3
+division= G/H
+print("la division es", division)
